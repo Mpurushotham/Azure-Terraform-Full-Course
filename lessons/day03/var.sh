d8e6c70b8bfb7b4ea98b306c306f2f7abb6becbe
Purushotham <purushotham.muktha@gmail.com>
2025-09-16 22:53:21 +0200

    added

--- lessons/day03/var.sh
+++
@@ -1,8 +0,0 @@
-
-#sample variables for terraform azure provider
-#export ARM_CLIENT_ID="437dff65-e689-4486-812f-e1d71bc5fc3b"
-#export ARM_CLIENT_SECRET="Fev8Q~5dX2hQmVAC_1_kQgVQzP3NLUDxosQcIcsu"
-#export ARM_SUBSCRIPTION_ID="36bd26b3-d5a3-4d75-9b42-68842c10325c"
-#export ARM_TENANT_ID="dcda7b6b-5239-42cd-a66a-a4289f839e3e"
-
-#az ad sp create-for-rbac -n az-demo --role="Contributor" --scopes="/subscriptions/$SUBSCRIPTION_ID"
